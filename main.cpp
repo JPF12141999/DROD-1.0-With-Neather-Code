7c2effdfc86f4bd2803c81e4c4752311e35e7897
JPF12141999 <149895098+JPF12141999@users.noreply.github.com>
2025-12-06 18:50:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,21 +14,14 @@ int main()
         pressed
     };
     ContinueButtonState ContinueState = ContinueButtonState::nothing;
-    bool Draw = false;
     sf::Texture MainMenu("Title.bmp");
     sf::Sprite MainMenuSprite(MainMenu);
     sf::Sprite Continue(MainMenu);
-    sf::Sprite ContinueHighlightSprite(MainMenu);
-    sf::Sprite ContinuePressedSprite(MainMenu);
     sf::IntRect ContinueRect({424, 86}, {126, 49});
     sf::IntRect ContinueHighlight({641, 27}, {117, 41});
     sf::IntRect ContinuePressed({641, 69}, {126, 50});
     Continue.setPosition({424.f, 86.f});
     Continue.setTextureRect(ContinueRect);
-    ContinueHighlightSprite.setPosition({430.f, 90.f});
-    ContinueHighlightSprite.setTextureRect(ContinueHighlight);
-    ContinuePressedSprite.setPosition({424.f, 86.f});
-    ContinuePressedSprite.setTextureRect(ContinuePressed);
     sf::Vector2f mousePos;
 
     while (window.isOpen())
@@ -50,18 +43,6 @@ int main()
                         ContinueState = ContinueButtonState::pressed;
                         clock.restart();
                     }
-                }
-            }
-
-            if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>())
-            {
-                if ((Continue.getGlobalBounds().contains(sf::Vector2f(mousePos)) && ContinueState != ContinueButtonState::pressed))
-                {
-                    Draw = true;
-                }
-                else
-                {
-                    Draw = false;
                 }
             }
 
@@ -88,11 +69,7 @@ int main()
             }
         }
 
-        if (Draw)
-        {
-            window.draw(ContinueHighlightSprite);
-        }
-
+        window.draw(Continue);
         window.display();
     }
 }
